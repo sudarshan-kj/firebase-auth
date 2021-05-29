9a4a9b69952f1c2c79c08bfad9e1dc6c7a5230e7
Sudarshan K J <kjsudi@gmail.com>
2021-05-29 21:38:13 +0530

    Integrating firebase auth

---
+++ src/components/Signup.js
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react";
+import { Card, Button, Form, Alert } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+
+const Signup = (props) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    console.log("Received");
+    e.preventDefault();
+    if (passwordRef.current.value !== passwordConfirmRef.current.value)
+      return setError("Passwords do not match");
+    try {
+      setError("");
+      setLoading(true);
+      await signup(emailRef.current.value, passwordRef.current.value);
+    } catch {
+      setError("Could not signup");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Signup</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" ref={emailRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Form.Group id="password-confirm">
+              <Form.Label>Password Confirmation</Form.Label>
+              <Form.Control type="password" ref={passwordConfirmRef} required />
+            </Form.Group>
+            <Button className="w-100 mt-3" type="submit" disabled={loading}>
+              Signup
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        Already have an account? Login
+      </div>
+    </>
+  );
+};
+
+export default Signup;
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// });
+
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBD_GjdMelNfjRTTqTwzJm_O84q5hzLHSc",
+  authDomain: "auth-react-dev-40571.firebaseapp.com",
+  projectId: "auth-react-dev-40571",
+  storageBucket: "auth-react-dev-40571.appspot.com",
+  messagingSenderId: "255467773521",
+  appId: "1:255467773521:web:8526c56945a61b288da270",
+});
+
+export const auth = app.auth();
+export default app;
